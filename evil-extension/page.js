079e9d7b91c168991d7933bb2ed40f56d34790ce
aqweteddy <keddy940199@gmail.com>
2019-04-29 21:48:03 +0800

    extension

--- evil-extension/page.js
+++ evil-extension/page.js
@@ -11,10 +11,23 @@ const MATCH_LIST = {
 };
 
 function transformTextNodes(node) {
+  // node = node.innerHTML
+  // console.log(node)
+  // for(var pat in MATCH_LIST){
+  //   // console.log(pat)
+  //   node = node.replace(pat, MATCH_LIST[pat])
+  // }
+  if (node.nodeType == Node.TEXT_NODE) {
+    for (var pat in MATCH_LIST)
+      node.textContent = node.textContent.replace(pat, MATCH_LIST[pat])
+  }
+  for (son of node.childNodes)
+    transformTextNodes(son)
   // TODO(you): Implement this function! See HW spec for details.
 }
 
-transformTextNodes(document.body);
+transformTextNodes(document.body)
 
 // Log statement to test that the extension loaded properly.
-console.log('Evil extension loaded!');
+console.log('Evil extension loaded!')
+console.log('Extension updated')
